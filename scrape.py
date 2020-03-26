3c38fa435aaf3d4c454bf0fe9829034a7c10001f
Felipe Pontes <felipemfpontes@gmail.com>
2020-03-26 20:09:55 -0300

    Add some logs during execution

--- scrape.py
+++ scrape.py
@@ -1,7 +1,15 @@
 from countries import scrape_countries
 from brazil import scrape_brazil
+import time
+
+def scrape(fn):
+    print(f'[{fn.__name__}] Starting...')
+    start_time = time.time()
+    fn()
+    elapsed_time = time.time() - start_time
+    print(f'[{fn.__name__}] Done in {elapsed_time:.3f}s!')
 
 
 if __name__ == "__main__":
-    scrape_countries()
-    scrape_brazil()
+    scrape(scrape_countries)
+    scrape(scrape_brazil)
