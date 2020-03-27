4e695650314cafc56c61b8a3796694bc4f33b49d
Felipe Pontes <felipemfpontes@gmail.com>
2020-03-27 09:33:22 -0300

    Add scrape output to scheduled commit message

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+mkdir -p ./tmp
 cd /home/felipemfp/Workspace/covid19-data
-/home/linuxbrew/.linuxbrew/bin/pipenv run python scrape.py
+/home/linuxbrew/.linuxbrew/bin/pipenv run python scrape.py > ./tmp/update-output.txt
 git add .
-git commit -m "Scheduled updates ($(date +'%F %T %Z'))"
+git commit -m "Scheduled updates ($(date +'%F %T %Z'))" -m "$(cat ./tmp/update-output.txt)"
 git push
