8fe34518e7702459936b481edf8ae1ea5243545a
Felipe Pontes <felipemfpontes@gmail.com>
2020-03-27 09:29:59 -0300

    Add timestamp to scheduled update commit message

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ set -e
 cd /home/felipemfp/Workspace/covid19-data
 /home/linuxbrew/.linuxbrew/bin/pipenv run python scrape.py
 git add .
-git commit -m "Updates"
+git commit -m "Scheduled updates ($(date +'%F %T %Z'))"
 git push
